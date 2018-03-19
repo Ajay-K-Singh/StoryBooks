53e0c9d92e59dcd0feabbb9c08ae1e67013cd7e2
Sammi-93 <aksingh1493@gmail.com>
2018-03-20 00:14:15 +0530

    Authorization using google auth

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
-//initialization of app with express
+const passport = require('passport');
+//Initialization of app with express
 const app = express();
+
+//Passport config
+require('./config/passport')(passport);
+
+//Load routes
+const auth = require('./routes/auth');
 
 app.get('/', (req, res) => {
     res.send('It works.');
 })
 
+
+//use routes
+app.use('/auth', auth);
+
+
+//Setting environment variables
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
