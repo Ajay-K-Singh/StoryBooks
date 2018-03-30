d760bc77177caa84ab40ee67133c947edf835133
Ajay Singh <aksingh1493@gmail.com>
2018-03-31 01:59:44 +0530

    Delete keys.js

--- config/keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb://ajay:ajay@ds149491.mlab.com:49491/storybooks-dev',
-    googleClientId: '241700356876-e2u0ir5ethdlhh6um21bbbi74ukpnhn5.apps.googleusercontent.com',
-    googleClientSecret: '75HmFhZ5mVm3WR_Xcm8ijcSN'
-}
